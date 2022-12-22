61e8c1019f63e4d82842923b3eab19861bf5a647
Myrela Almeida <67117318+myra-ai@users.noreply.github.com>
2022-12-22 03:32:08 -0300

    Update update.php

--- update.php
+++ update.php
@@ -9,7 +9,7 @@
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Editar Usuários/title>
+    <title>Editar Usuários</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
